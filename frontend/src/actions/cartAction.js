04d95f1ebc8bf866ef1453eb3d0730292dc09862
ZeenatFirdosh <zeenatzfq052@gmail.com>
2023-10-16 18:20:05 +0530

    /api/v1

--- frontend/src/actions/cartAction.js
+++ frontend/src/actions/cartAction.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 // Add to Cart
 export const addItemsToCart = (id, quantity) => async (dispatch, getState) => {
-  const { data } = await axios.get(`/api/v1/product/${id}`);
+  const { data } = await axios.get(`${process.env.BACKEND_URL}/api/v1/product/${id}`);
 
   dispatch({
     type: ADD_TO_CART,
